739801e278c3bdd41c43fa3ac8423fec7befa8b3
Markus Pilman <mpilman@inf.ethz.ch>
2016-02-07 09:39:27 +0100

    removed Java implementation

--- presto-tell/src/main/java/ch/ethz/tell/presto/java/PrimaryKeyColumnHandle.java
+++
@@ -1,6 +0,0 @@
-package ch.ethz.tell.presto.java;
-
-import com.facebook.presto.spi.ColumnHandle;
-
-public class PrimaryKeyColumnHandle implements ColumnHandle {
-}
--- presto-tell/src/main/java/ch/ethz/tell/presto/java/TellTableLayoutHandle.java
+++
@@ -1,6 +0,0 @@
-package ch.ethz.tell.presto.java;
-
-import com.facebook.presto.spi.ConnectorTableLayoutHandle;
-
-public class TellTableLayoutHandle implements ConnectorTableLayoutHandle {
-}
